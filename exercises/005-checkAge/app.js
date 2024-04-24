800ad589920ad50e0a215f4b42b7eced4b8fea0a
Alejandro Núñez <145801334+nunezweb@users.noreply.github.com>
2024-04-24 08:29:23 +0000

    Pending changes exported from your codespace

--- exercises/005-checkAge/app.js
+++ exercises/005-checkAge/app.js
@@ -1,4 +1,8 @@
+let output = checkAge('Adrian', 22);
+
 function checkAge(name, age) {
   // your code here
-  
+  if(age < 21) return `Go home, ${name}!`
+  else return `Welcome, ${name}!`
 }
+console.log(output); // --> 'Welcome, Adrian!'
--- exercises/006-getFullName/app.js
+++ exercises/006-getFullName/app.js
@@ -1,4 +1,8 @@
+let output = getFullName('Joe', 'Smith');
+
 function getFullName(firstName, lastName) {
   // your code here
-  
+  return firstName + ' ' + lastName
 }
+
+console.log(output); // --> 'Joe Smith'
